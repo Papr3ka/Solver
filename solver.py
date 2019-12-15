34dfca0ebc416ec15254ff78f9da9caa261fdf1d
Benjamin <58241833+Papr3ka@users.noreply.github.com>
2019-12-15 16:12:25 -0500

    Update solver.py

--- solver.py
+++ solver.py
@@ -22,85 +22,95 @@ def wait():
         wait = str(input("Press enter to exit..."))
 def equation_edit():
     global equation_str
+    global original_eq_str
     global eq_range_max
     global eq_range_min
-    equation_str = equation_str.replace("^","**")
-    equation_str = equation_str.replace("0(", "0*(")
-    equation_str = equation_str.replace("1(", "1*(")
-    equation_str = equation_str.replace("2(", "2*(")
-    equation_str = equation_str.replace("3(", "3*(")
-    equation_str = equation_str.replace("4(", "4*(")
-    equation_str = equation_str.replace("5(", "5*(")
-    equation_str = equation_str.replace("6(", "6*(")
-    equation_str = equation_str.replace("7(", "7*(")
-    equation_str = equation_str.replace("8(", "8*(")
-    equation_str = equation_str.replace("9(", "9*(")
-    equation_str = equation_str.replace("0x", "0*x")
-    equation_str = equation_str.replace("1x", "1*x")
-    equation_str = equation_str.replace("2x", "2*x")
-    equation_str = equation_str.replace("3x", "3*x")
-    equation_str = equation_str.replace("4x", "4*x")
-    equation_str = equation_str.replace("5x", "5*x")
-    equation_str = equation_str.replace("6x", "6*x")
-    equation_str = equation_str.replace("7x", "7*x")
-    equation_str = equation_str.replace("8x", "8*x")
-    equation_str = equation_str.replace("9x", "9*x")
-    equation_str = equation_str.replace("arcsinh", "trig_f03")
-    equation_str = equation_str.replace("arccosh", "trig_f13")
-    equation_str = equation_str.replace("arctanh", "trig_f23")
-    equation_str = equation_str.replace("arcsin", "trig_f01")
-    equation_str = equation_str.replace("arccos", "trig_f11")
-    equation_str = equation_str.replace("arctan", "trig_f21")
-    equation_str = equation_str.replace("sinh", "trig_f02")
-    equation_str = equation_str.replace("cosh", "trig_f12")
-    equation_str = equation_str.replace("tanh", "trig_f22")
-    equation_str = equation_str.replace("sin", "trig_f00")
-    equation_str = equation_str.replace("cos", "trig_f10")
-    equation_str = equation_str.replace("tan", "trig_f20")
-    equation_str = equation_str.replace("trig_f03", "math.asinh")
-    equation_str = equation_str.replace("trig_f13", "math.acosh")
-    equation_str = equation_str.replace("trig_f23", "math.atanh")
-    equation_str = equation_str.replace("trig_f01", "math.asin")
-    equation_str = equation_str.replace("trig_f11", "math.acos")
-    equation_str = equation_str.replace("trig_f21", "math.atan")
-    equation_str = equation_str.replace("trig_f02", "math.sinh")
-    equation_str = equation_str.replace("trig_f12", "math.cosh")
-    equation_str = equation_str.replace("trig_f22", "math.tanh")
-    equation_str = equation_str.replace("trig_f00", "math.sin")
-    equation_str = equation_str.replace("trig_f10", "math.cos")
-    equation_str = equation_str.replace("trig_f20", "math.tan")
-    if equation_str.count(".asin") and eq_range_max > 1:
-        eq_range_max = 1
-    if equation_str.count(".asin") and eq_range_min < -1:
-        eq_range_min = -1
-    equation_str = equation_str.replace("arccos", "math.acos")
-    if equation_str.count(".acos") and eq_range_max > 1:
-        eq_range_max = 1
-    if equation_str.count(".acos") and eq_range_min < -1:
-        eq_range_min = -1
-    equation_str = equation_str.replace("arctan", "math.atan")
-    if equation_str.count(".atan") and eq_range_max > 1:
-        eq_range_max = 1
-    if equation_str.count(".atan") and eq_range_min < -1:
-        eq_range_min = -1
-    equation_str = equation_str.replace("abs", "math.abs")
-    equation_str = equation_str.replace("floor", "math.floor")
-    equation_str = equation_str.replace("ceil", "math.ceil")
-    equation_str = equation_str.replace("factorial", "math.factorial")
-    equation_str = equation_str.replace("gamma", "math.gamma")
-    equation_str = equation_str.replace("sqrt", "math.sqrt")
+    for x in range(1,len(str(original_eq_str)),1):
+        equation_str = equation_str.replace("^","**")
+        equation_str = equation_str.replace("xx", "x*x")
+        equation_str = equation_str.replace("0(", "0*(")
+        equation_str = equation_str.replace("1(", "1*(")
+        equation_str = equation_str.replace("2(", "2*(")
+        equation_str = equation_str.replace("3(", "3*(")
+        equation_str = equation_str.replace("4(", "4*(")
+        equation_str = equation_str.replace("5(", "5*(")
+        equation_str = equation_str.replace("6(", "6*(")
+        equation_str = equation_str.replace("7(", "7*(")
+        equation_str = equation_str.replace("8(", "8*(")
+        equation_str = equation_str.replace("9(", "9*(")
+        equation_str = equation_str.replace("0x", "0*x")
+        equation_str = equation_str.replace("1x", "1*x")
+        equation_str = equation_str.replace("2x", "2*x")
+        equation_str = equation_str.replace("3x", "3*x")
+        equation_str = equation_str.replace("4x", "4*x")
+        equation_str = equation_str.replace("5x", "5*x")
+        equation_str = equation_str.replace("6x", "6*x")
+        equation_str = equation_str.replace("7x", "7*x")
+        equation_str = equation_str.replace("8x", "8*x")
+        equation_str = equation_str.replace("9x", "9*x")
+        equation_str = equation_str.replace("arcsinh", "trig_f03")
+        equation_str = equation_str.replace("arccosh", "trig_f13")
+        equation_str = equation_str.replace("arctanh", "trig_f23")
+        equation_str = equation_str.replace("arcsin", "trig_f01")
+        equation_str = equation_str.replace("arccos", "trig_f11")
+        equation_str = equation_str.replace("arctan", "trig_f21")
+        equation_str = equation_str.replace("sinh", "trig_f02")
+        equation_str = equation_str.replace("cosh", "trig_f12")
+        equation_str = equation_str.replace("tanh", "trig_f22")
+        equation_str = equation_str.replace("sin", "trig_f00")
+        equation_str = equation_str.replace("cos", "trig_f10")
+        equation_str = equation_str.replace("tan", "trig_f20")
+        equation_str = equation_str.replace("trig_f03", "math.asinh")
+        equation_str = equation_str.replace("trig_f13", "math.acosh")
+        equation_str = equation_str.replace("trig_f23", "math.atanh")
+        equation_str = equation_str.replace("trig_f01", "math.asin")
+        equation_str = equation_str.replace("trig_f11", "math.acos")
+        equation_str = equation_str.replace("trig_f21", "math.atan")
+        equation_str = equation_str.replace("trig_f02", "math.sinh")
+        equation_str = equation_str.replace("trig_f12", "math.cosh")
+        equation_str = equation_str.replace("trig_f22", "math.tanh")
+        equation_str = equation_str.replace("trig_f00", "math.sin")
+        equation_str = equation_str.replace("trig_f10", "math.cos")
+        equation_str = equation_str.replace("trig_f20", "math.tan")
+        if equation_str.count(".asin") and eq_range_max > 1:
+            eq_range_max = 1
+        if equation_str.count(".asin") and eq_range_min < -1:
+            eq_range_min = -1
+        equation_str = equation_str.replace("arccos", "math.acos")
+        if equation_str.count(".acos") and eq_range_max > 1:
+            eq_range_max = 1
+        if equation_str.count(".acos") and eq_range_min < -1:
+            eq_range_min = -1
+        equation_str = equation_str.replace("arctan", "math.atan")
+        if equation_str.count(".atan") and eq_range_max > 1:
+            eq_range_max = 1
+        if equation_str.count(".atan") and eq_range_min < -1:
+            eq_range_min = -1
+        equation_str = equation_str.replace("abs", "math.fabs")
+        equation_str = equation_str.replace("floor", "math.floor")
+        equation_str = equation_str.replace("ceil", "math.ceil")
+        equation_str = equation_str.replace("gamma" or "factorial", "math.gamma")
+        equation_str = equation_str.replace("sqrt", "math.sqrt")
 def solve():
     clear()
+    global tstart
+    global tend
     global x_var
     global y_var
     y_var = 0
     x_var = eq_range_min
     print("X","		   Y")
+    tstart = time.perf_counter()
     while x_var <= eq_range_max:
         x = x_var
-        y_var = eval(equation_str)
-        print(x_var,"		|",y_var)
-        x_var += eq_res
+        try:
+            eval(equation_str)
+            y_var = dec(eval(equation_str))
+            print(x_var,"		|",y_var)
+            x_var += eq_res
+        except (ValueError, ZeroDivisionError):
+            x_var += eq_res
+    tend = time.perf_counter()
 clear()
 getcontext().prec = 96
 equation_str = "0"
@@ -112,10 +122,16 @@ eq_range_min = dec(input("Min X:"))
 eq_range_max = dec(input("Max X:"))
 eq_res = dec(input("Resolution:"))
 original_eq_str = equation_str
+clear()
+print("Preparing...")
 equation_edit()
 solve()
 print("\n")
 print(original_eq_str)
 print("\n")
+print("Time", tend - tstart)
+print("\n")
 waitsetc = 0
 wait()
+clear()
+# End of Program
